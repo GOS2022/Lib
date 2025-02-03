b60e06a9768f1733d0d9d5f15ab941053660f85e
Ahmed Gazar <118141346+gaborr971@users.noreply.github.com>
2025-02-03 11:21:31 +0100

    Lib files added.

---
+++ GOS2022_lib/Debug/svl/src/svl_ers.d
@@ -0,0 +1,45 @@
+svl/src/svl_ers.o: ../svl/src/svl_ers.c \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022_lib/svl/inc/svl_ers.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_driver.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_crc_driver.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/kernel/inc/gos_kernel.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/kernel/inc/gos_config.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_trace_driver.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_shell_driver.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_sysmon_driver.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_timer_driver.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_error.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_trace.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_gcp.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_message.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_mutex.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_queue.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_shell.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_signal.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_sysmon.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_time.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_trigger.h \
+ C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022_lib/svl/inc/svl_cfg.h
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022_lib/svl/inc/svl_ers.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_driver.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_crc_driver.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/kernel/inc/gos_kernel.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/kernel/inc/gos_config.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_trace_driver.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_shell_driver.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_sysmon_driver.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/driver/inc/gos_timer_driver.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_error.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_trace.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_gcp.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_message.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_mutex.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_queue.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_shell.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_signal.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_sysmon.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_time.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022/os/services/inc/gos_trigger.h:
+C:/Users/Ahmed/Documents/GitHub/GOS2022/Projects/Bootloader/GOS2022_lib/svl/inc/svl_cfg.h:
---
+++ README.md
@@ -0,0 +1,24 @@
+# GOS2022_lib
+This repository contains the source code of GOS2022 library.
+
+# Description
+This library contains drivers and services aimed to provide a universal software package that can be
+used on GOS-based devices, and that is compatible with features provided by GOS tool. 
+
+# Content
+The repository currently contains the latest stable version of the library.
+
+GOS2022_lib provides the following features:
+- Driver package including peripheral drivers as well as drivers for some commonly used chips (such as
+I/O extender, memory chips).
+- Bootloader generic part
+- Service layer package including generic services (device state manager, event reporting,
+inter-processor link for WeMos communication for wireless services, montoring data, project data handler,
+sofware download handler, etc.)
+
+# Remarks
+Contribution to this project is encouraged and appriciated.
+If you have any questions or ideas, feel free to contact me: gaborr97@gmail.com
+
+Last updated: 2025-02-03
+(c) 2025, Ahmed Gazar
